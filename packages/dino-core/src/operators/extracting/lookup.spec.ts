41f0bb3f9923d9891d2372abec0ef10b808111a3
axdanbol <daniel_bolin@yahoo.com>
2018-03-21 15:41:21 -0400

    Added tests

---
+++ packages/dino-core/src/operators/extracting/lookup.spec.ts
@@ -0,0 +1,11 @@
+/// <reference path="../../../../../node_modules/@types/jasmine/index.d.ts" />
+
+import { LookupOperator } from './lookup';
+
+describe('operators', () => { // Prevent deep indentation
+describe('extracting', () => { // Prevent deep indentation
+describe('LookupOperator', () => {
+ // TODO
+});
+});
+});

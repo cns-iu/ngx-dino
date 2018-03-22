f81368bac5937cf4612e5eb28f3915fe88e74eae
axdanbol <daniel_bolin@yahoo.com>
2018-03-22 14:19:22 -0400

    Field v2

---
+++ packages/dino-core/src/v2/field.ts
@@ -0,0 +1,61 @@
+import { Map } from 'immutable';
+
+import { Operator } from '../operators';
+
+
+export enum DataType {
+  Any = 'any'
+}
+
+
+type MappingArg<In, Out> = Iterable<[string, Operator<In, Out>]> |
+  {[key: string]: Operator<In, Out>};
+
+export interface FieldArgs<R> {
+  id: string;
+  label: string;
+  datatype?: DataType;
+
+  initialOp?: Operator<any, any>;
+  mapping?: MappingArg<any, R>;
+}
+
+
+export class Field<T> {
+  readonly id: string;
+  readonly label: string;
+  readonly datatype: DataType;
+
+  readonly initialOp: Operator<any, any>;
+  readonly mapping: Map<string, BoundField<T>>;
+
+  constructor(args: FieldArgs<T>) {
+    ({
+      id: this.id,
+      label: this.label,
+      datatype: this.datatype = DataType.Any,
+      initialOp: this.initialOp = Operator.identity()
+    } = args);
+
+    this.mapping = Map(Map(args.mapping || {}).map((op: Operator<any, T>) => {
+      return new BoundField(this, this.initialOp.chain(op));
+    }));
+  }
+
+  getOperator(selector: string): Operator<any, T> {
+    const bound = this.getBoundField(selector);
+    return bound && bound.operator;
+  }
+
+  getBoundField(selector: string): BoundField<T> {
+    return this.mapping.get(selector);
+  }
+}
+
+export class BoundField<T> {
+  constructor(readonly field: Field<T>, readonly operator: Operator<any, T>) { }
+
+  get(data: any): T {
+    return this.operator.get(data);
+  }
+}

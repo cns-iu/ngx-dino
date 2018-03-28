70cded792bdbeb54816873777cdabebebb4b3295
axdanbol <daniel_bolin@yahoo.com>
2018-03-28 16:23:08 -0400

    Improved MapOperator to take arbitrary arguments to forward to the mapper

--- packages/dino-core/src/operators/transforming/map.spec.ts
+++ packages/dino-core/src/operators/transforming/map.spec.ts
@@ -12,6 +12,14 @@ describe('MapOperator', () => {
     expect(op.mapper).toBe(mapper);
   });
 
+  it('should set an args', () => {
+    const mapper = () => undefined;
+    const args = [1, 2, 7, 2, 1];
+    const op = new MapOperator(mapper, ...args);
+
+    expect(op.args).toBeDefined();
+  });
+
   it('should call the mapper', () => {
     const mapper = jasmine.createSpy();
     const op = new MapOperator(mapper);
@@ -28,10 +36,21 @@ describe('MapOperator', () => {
     expect(op.get(undefined)).toBe(value);
   });
 
-  it('should be equal if same mapper', () => {
+  it('should send the arguments to the mapper', () => {
+    const mapper = jasmine.createSpy();
+    const data = 123;
+    const args: any[] = ['a', 22, [1, 2]];
+    const op = new MapOperator(mapper, ...args);
+
+    op.get(data);
+    expect(mapper).toHaveBeenCalledWith(...[data].concat(args));
+  });
+
+  it('should be equal if same mapper and arguments', () => {
     const mapper = () => undefined;
-    const op1 = new MapOperator(mapper);
-    const op2 = new MapOperator(mapper);
+    const args = [{p: 'rr'}, 'tt', 2];
+    const op1 = new MapOperator(mapper, ...args);
+    const op2 = new MapOperator(mapper, ...args);
 
     expect(op1.equals(op2)).toBeTruthy();
   });
@@ -44,6 +63,20 @@ describe('MapOperator', () => {
     const op2 = new MapOperator(mapper2);
 
     expect(op1.equals(op2)).toBeFalsy();
+  });
+
+  it('should not be equal if different arguments', () => {
+    const mapper = () => 0;
+    const args1 = [{}, 'b', 'c'];
+    const args2 = ['d', [1], 9];
+    const args3 = [{}, 'b', 'c']; // Different object than args1!
+
+    const op1 = new MapOperator(mapper, ...args1);
+    const op2 = new MapOperator(mapper, ...args2);
+    const op3 = new MapOperator(mapper, ...args2);
+
+    expect(op1.equals(op2)).toBeFalsy();
+    expect(op1.equals(op3)).toBeFalsy();
   });
 });
 });

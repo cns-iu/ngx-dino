a03949969da9f7dfa673f0c516efb28fe4ccb73f
axdanbol <daniel_bolin@yahoo.com>
2018-03-29 14:37:04 -0400

    Implemented working datatable

--- packages/dino-datatable/src/datatable/datatable.component.ts
+++ packages/dino-datatable/src/datatable/datatable.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component, Input,
+  OnInit, OnChanges,
+  SimpleChanges
+} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { BoundField } from '@ngx-dino/core';
+
+import { DatatableService } from '../shared/datatable.service';
+
 
 @Component({
-  selector: 'aisl-datatable',
+  selector: 'dino-datatable',
   templateUrl: './datatable.component.html',
   styleUrls: ['./datatable.component.sass']
 })
-export class DatatableComponent implements OnInit {
+export class DatatableComponent implements OnInit, OnChanges {
+  @Input() data: Observable<any[]>;
+  @Input() fields: BoundField<any>[];
 
-  constructor() { }
-
-  ngOnInit() {
+  dataSource: Observable<any[][]>;
+  get columns(): string[] {
+    return this.fields.map((f) => f.field.label);
   }
 
+  constructor(private service: DatatableService) { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.dataSource = this.service.makeDataSource(this.data, this.fields);
+  }
 }

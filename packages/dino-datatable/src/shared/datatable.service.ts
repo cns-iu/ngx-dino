c60d1a83ccbb8dd886cb5e82608918c7d4cb464d
axdanbol <daniel_bolin@yahoo.com>
2018-04-09 15:07:17 -0400

    Added support for links in datatable

--- packages/dino-datatable/src/shared/datatable.service.ts
+++ packages/dino-datatable/src/shared/datatable.service.ts
@@ -4,14 +4,16 @@ import { Observable } from 'rxjs/Observable';
 import { Operator, BoundField } from '@ngx-dino/core';
 import '@ngx-dino/core/src/operators/add/static/combine';
 
+import { DataType } from './data-types';
+
 
 @Injectable()
 export class DatatableService {
   constructor() { }
 
   makeDataSource(
-    data: Observable<any[]>, fields: BoundField<any>[]
-  ): Observable<any[][]> {
+    data: Observable<any[]>, fields: BoundField<DataType>[]
+  ): Observable<DataType[][]> {
     const op = Operator.combine<any, any[]>(fields.map((f) => f.operator));
     return data.map((items) => items.map(op.getter));
   }

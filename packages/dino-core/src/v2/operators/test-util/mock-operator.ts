c74e47ba32810c7ad5c3d3aa410f60b87533de0f
axdanbol <daniel_bolin@yahoo.com>
2018-04-10 11:47:18 -0400

    Testing utility operator

--- packages/dino-core/src/v2/operators/test-util/mock-operator.ts
+++ packages/dino-core/src/v2/operators/test-util/mock-operator.ts
@@ -1,18 +1,18 @@
-import { Collection } from 'immutable';
+import { Collection, List } from 'immutable';
 import { isFunction } from 'lodash';
 
-import { OperatorFlags } from '../base/operator-flags';
-import { BaseOperator } from '../base/base-operator';
+import { Flags } from '../base/flags';
+import { BaseOperator, BaseCache } from '../base/base';
 
 
 export class MockOperator<In = any, Out = any> extends BaseOperator<In, Out> {
-  readonly value: (data: In) => Out;
+  readonly value: (data: In, cache: BaseCache) => Out;
   readonly state: () => Collection<any, any>;
 
   constructor(
-    value?: ((data: In) => Out) | Out,
-    state?: (() => Collection<any, any>) | Collection<any, any>,
-    flags: OperatorFlags = OperatorFlags.None
+    value?: ((data: In, cache: BaseCache) => Out) | Out,
+    state: (() => Collection<any, any>) | Collection<any, any> = List(),
+    flags: Flags = Flags.None
   ) {
     super(flags);
 
@@ -21,8 +21,8 @@ export class MockOperator<In = any, Out = any> extends BaseOperator<In, Out> {
   }
 
 
-  public getImpl(data: In): Out {
-    return this.value(data);
+  public getImpl(data: In, cache: BaseCache): Out {
+    return this.value(data, cache);
   }
 
   public getStateImpl(): Collection<any, any> {

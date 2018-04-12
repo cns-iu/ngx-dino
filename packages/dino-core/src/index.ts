9ff1f977c69e5ddbb97a4fdcbfa175ab86e7901f
axdanbol <daniel_bolin@yahoo.com>
2018-04-12 10:33:55 -0400

    Exported adapter

--- packages/dino-core/src/index.ts
+++ packages/dino-core/src/index.ts
@@ -9,3 +9,4 @@ export { StreamCache } from './shared/stream-cache';
 export { Operator, Processor } from './operators';
 import { Field as FieldV2, BoundField } from './v2/field';
 export { FieldV2, BoundField };
+export { BoundFieldAdapter, adaptBoundField } from './v2/field-adapter';

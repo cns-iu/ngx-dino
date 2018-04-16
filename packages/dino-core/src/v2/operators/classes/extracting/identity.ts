5042b8cc7b85ca47d96fad5b11a661296130fad4
axdanbol <daniel_bolin@yahoo.com>
2018-04-16 16:25:10 -0400

    Added IdentityOperator w/ tests

---
+++ packages/dino-core/src/v2/operators/classes/extracting/identity.ts
@@ -0,0 +1,23 @@
+import { Collection, List } from 'immutable';
+
+import { Flags } from '../../base/flags';
+import { BaseOperator, BaseCache } from '../../base/base';
+
+
+const flags = Flags.combine(Flags.Stateless, Flags.SideEffectFree);
+const state = List.of({});
+
+
+export class IdentityOperator<T> extends BaseOperator<T, T> {
+  constructor() {
+    super(flags);
+  }
+
+  getImpl(data: T, cache: BaseCache): T {
+    return data;
+  }
+
+  getStateImpl(): Collection<any, any> {
+    return state;
+  }
+}

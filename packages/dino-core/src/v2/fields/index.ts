dd9b5f8bde3392e8b6484efa8ae11c93113d0ebe
axdanbol <daniel_bolin@yahoo.com>
2018-04-26 12:42:43 -0400

    Added multi field with support for pre and post operations

--- packages/dino-core/src/v2/fields/index.ts
+++ packages/dino-core/src/v2/fields/index.ts
@@ -1,4 +1,6 @@
 export { DataType, FieldArgs, Field } from './base/field';
 export { BoundField } from './base/bound-field';
 
-export { SimpleField } from './field-classes/simple-field';
+export { SimpleFieldArgs, SimpleField } from './field-classes/simple-field';
+export { MultiFieldArgs, MultiField } from './field-classes/multi-field';
+export { PrePostMultiFieldArgs, PrePostMultiField } from './field-classes/pre-post-multi-field';

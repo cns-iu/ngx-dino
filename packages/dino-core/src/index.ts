43656823cf371ca2c03583552684309ca696555f
axdanbol <daniel_bolin@yahoo.com>
2018-05-01 15:54:21 -0400

    Added to index

--- packages/dino-core/src/index.ts
+++ packages/dino-core/src/index.ts
@@ -1,5 +1,8 @@
 import './rxjs-operators';
-import { Field as FieldV2, BoundField, simpleField } from './v2/fields';
+import {
+  Field as FieldV2, BoundField,
+  simpleField, multiField, prePostMultiField
+} from './v2/fields';
 
 export { DinoCoreModule } from './dino-core.module';
 export { Changes, DatumId, isDatumId } from './shared/changes';
@@ -8,5 +11,5 @@ export { FieldProcessor } from './shared/field-processor';
 export { StreamCache } from './shared/stream-cache';
 
 export { Operator } from './v2/operators';
-export { FieldV2, BoundField, simpleField };
+export { FieldV2, BoundField, simpleField, multiField, prePostMultiField };
 export { BoundFieldAdapter, adaptBoundField } from './v2/field-adapter';

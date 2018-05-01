504fec3b378435d8661cb75832daf4f8fbb777ac
axdanbol <daniel_bolin@yahoo.com>
2018-05-01 10:27:14 -0400

    Moved stand alone creation functions to utility

--- packages/dino-core/src/v2/fields/index.ts
+++ packages/dino-core/src/v2/fields/index.ts
@@ -1,5 +1,5 @@
 export { DataType, Field } from './base/field';
 export { BoundField } from './base/bound-field';
 
-export { simpleField } from './classes/simple-field';
-export { multiField, prePostMultiField } from './classes/multi-field';
+export { simpleField } from './utility/simple-field';
+export { multiField, prePostMultiField } from './utility/multi-field';

100f679cb930f21d2b608e8229d89ac08a29c0af
axdanbol <daniel_bolin@yahoo.com>
2018-05-04 14:11:03 -0400

    multiField tests skeleton

---
+++ packages/dino-core/src/v2/fields/factories/multi-field.spec.ts
@@ -0,0 +1,30 @@
+import immutableEqualityTester from '../../test-utility/equality/immutable';
+import oneOfMatchers from '../../test-utility/matchers/one-of';
+
+import { constant } from '../../operators/methods/generating/constant';
+
+import { DataType, Field } from '../field';
+import { BoundField } from '../bound-field';
+import { multiField, prePostMultiField } from './multi-field';
+
+
+describe('fields', () => {
+describe('factories', () => {
+describe('multiField', () => {
+  beforeEach(() => {
+    // Add equality testers
+    jasmine.addCustomEqualityTester(immutableEqualityTester);
+  });
+  // TODO
+});
+
+
+describe('prePostMultiField', () => {
+  beforeEach(() => {
+    // Add equality testers
+    jasmine.addCustomEqualityTester(immutableEqualityTester);
+  });
+  // TODO
+});
+});
+});

f673f05073a06cb942f76d1404d807bee64d94f4
Gagandeep Singh <singhgag@indiana.edu>
2018-07-24 13:48:26 -0400

    ForceNetwork(WIP)

---
+++ projects/force-network/src/lib/shared/network.ts
@@ -0,0 +1,17 @@
+export interface Node {
+  id: string;
+  [prop: string]: any;
+}
+
+
+export interface Link {
+  id: string;
+  source: string;
+  target: string;
+  [prop: string]: any;
+}
+
+export interface Graph {
+  nodes: Node[],
+  edges: Link[]
+}
--- projects/force-network/src/public_api.ts
+++ projects/force-network/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of force-network
  */
 
-export * from './lib/force-network.service';
 export * from './lib/force-network.component';
 export * from './lib/force-network.module';

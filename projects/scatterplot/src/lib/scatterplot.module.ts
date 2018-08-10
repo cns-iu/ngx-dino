58b50aba9d5dd2f05722671ab4dc27ffd53478e2
axdanbol <daniel_bolin@yahoo.com>
2018-08-10 13:08:37 -0400

    Working autoresize for scatterplot

--- projects/scatterplot/src/lib/scatterplot.module.ts
+++ projects/scatterplot/src/lib/scatterplot.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CoreModule } from '@ngx-dino/core';
 import { ScatterplotComponent } from './scatterplot.component';
 
 @NgModule({
-  imports: [
-  ],
+  imports: [CoreModule],
   declarations: [ScatterplotComponent],
   exports: [ScatterplotComponent]
 })

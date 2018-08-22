1e200527d925a0f52ffd028baa60fb36ae91cf2f
Bruce Herr II <bherr@indiana.edu>
2018-08-22 12:32:47 -0400

    Import Operator to make ng-packager generate d.ts files correctly.

--- projects/science-map/src/lib/shared/journal-name-lookup.ts
+++ projects/science-map/src/lib/shared/journal-name-lookup.ts
@@ -1,7 +1,7 @@
-import { chain, map, lookup } from '@ngx-dino/core';
+import { chain, map, lookup, Operator } from '@ngx-dino/core';
 
 import * as journalNameToIdLookup from '../data/journalNameToId.data';
 import { normalizeJournalName } from './normalize-journal-name';
 
-export const journalNameRawLookup = lookup(journalNameToIdLookup);
-export const journalNameLookup = chain(map(normalizeJournalName), journalNameRawLookup);
+export const journalNameRawLookup: Operator<string, any> = lookup(journalNameToIdLookup);
+export const journalNameLookup: Operator<string, any> = chain(map(normalizeJournalName), journalNameRawLookup);

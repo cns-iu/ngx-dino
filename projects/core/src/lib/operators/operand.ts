73e1239c8164b783bc3cb5fb4ff8b9bc30cff3ae
Bruce Herr II <bherr@indiana.edu>
2018-09-13 09:36:29 -0400

    Added default caching to the @Operand decorator, bumped to v0.7.2

--- projects/core/src/lib/operators/operand.ts
+++ projects/core/src/lib/operators/operand.ts
@@ -1,12 +1,30 @@
 import { Operator } from './operator';
 
-export function Operand<T = any>(operator: Operator<any, T>) {
+export function Operand<T = any>(operator: Operator<any, T>, cached = true) {
   return function (target: any, key: string) {
     if (delete target[key]) {
+      let getter = function() { operator.get(this); };
+
+      if (cached) {
+        const cacheKey = `__MEMOIZED_${key}__`;
+        getter = function() {
+          if (this.hasOwnProperty(cacheKey)) {
+            return this[cacheKey];
+          } else {
+            const result = operator.get(this);
+            Object.defineProperty(this, cacheKey, {
+              configurable: false,
+              enumerable: false,
+              writable: false,
+              value: result
+            });
+            return result;
+          }
+        }
+      }
+
       Object.defineProperty(target, key, {
-        get: function() {
-          return operator.get(this);
-        },
+        get: getter,
         enumerable: true,
         configurable: true
       });

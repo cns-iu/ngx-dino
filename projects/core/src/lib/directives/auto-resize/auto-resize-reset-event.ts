db1cca0353a3f48e7f567e217c28e9aec1622faf
Bruce Herr II <bherr@indiana.edu>
2018-09-24 11:11:31 -0400

    Minor patch to fix running @ngx-dino/core in node.js

--- projects/core/src/lib/directives/auto-resize/auto-resize-reset-event.ts
+++ projects/core/src/lib/directives/auto-resize/auto-resize-reset-event.ts
@@ -4,13 +4,17 @@ export interface AutoResizeResetEventConstructor {
   readonly eventName: string;
 }
 
+const BaseEvent = typeof Event !== 'undefined' ? Event : <any>class Event {
+  constructor(readonly name: string, readonly options?: any) { }
+}
+
 const eventName = 'auto-resize-reset';
 let constructor: AutoResizeResetEventConstructor;
 let prototype: any;
 
 // Old style class due to the way typescript inheritance is transpiled.
 function AutoResizeResetEventConstructorFunction(options: EventInit = {}): AutoResizeResetEvent {
-  const ev = new Event(eventName, Object.assign({ bubbles: true }, options));
+  const ev = new BaseEvent(eventName, Object.assign({ bubbles: true }, options));
   Object.setPrototypeOf(ev, prototype);
   return ev;
 }
@@ -18,7 +22,7 @@ function AutoResizeResetEventConstructorFunction(options: EventInit = {}): AutoR
 constructor = AutoResizeResetEventConstructorFunction as any;
 (constructor as any).eventName = eventName;
 
-prototype = constructor.prototype = Object.create(Event.prototype);
+prototype = constructor.prototype = Object.create(BaseEvent.prototype);
 prototype.constructor = constructor;
 
 export const AutoResizeResetEvent = constructor;

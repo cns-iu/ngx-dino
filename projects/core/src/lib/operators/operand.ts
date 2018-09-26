b15619b9faac708f2e54f62f3f4980b04da252e7
axdanbol <daniel_bolin@yahoo.com>
2018-09-26 11:15:48 -0400

    Bug fix - transpiled code incorrectly removed a return statement

--- projects/core/src/lib/operators/operand.ts
+++ projects/core/src/lib/operators/operand.ts
@@ -3,11 +3,14 @@ import { Operator } from './operator';
 export function Operand<T = any>(operator: Operator<any, T>, cached = true) {
   return function (target: any, key: string) {
     if (delete target[key]) {
-      let getter = function() { return operator.get(this); };
+      let getter = function(): T {
+        const result = operator.get(this);
+        return result;
+      };
 
       if (cached) {
         const cacheKey = `__MEMOIZED_${key}__`;
-        getter = function() {
+        getter = function(): T {
           if (this.hasOwnProperty(cacheKey)) {
             return this[cacheKey];
           } else {

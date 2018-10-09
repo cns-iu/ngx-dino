ddae92a114cc3ccbd46e44f176b7afbf55c413d2
axdanbol <daniel_bolin@yahoo.com>
2018-10-09 14:41:52 -0400

    Exported science map component in public api

--- projects/science-map/src/public_api.ts
+++ projects/science-map/src/public_api.ts
@@ -3,6 +3,7 @@
  */
 
 export * from './lib/science-map.module';
+export * from './lib/science-map.component';
 
 export * from './lib/shared/issn-journal-lookup';
 export * from './lib/shared/journal-name-lookup';

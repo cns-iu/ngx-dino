b3f0f1bb4ad553b955fc25e4f780f5d18ad2ecdd
Bruce Herr II <bherr@indiana.edu>
2018-10-18 17:21:49 -0400

    Export component classes for legend and bumped legend version

--- projects/legend/src/public_api.ts
+++ projects/legend/src/public_api.ts
@@ -3,3 +3,7 @@
  */
 
 export * from './lib/legend.module';
+
+export * from './lib/color-legend/color-legend.component';
+export * from './lib/node-size-legend/node-size-legend.component';
+export * from './lib/edge-size-legend/edge-size-legend.component';

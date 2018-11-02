18314ebd992c685f1ac19a42084849874ce6d0f1
axdanbol <daniel_bolin@yahoo.com>
2018-11-02 12:56:28 -0400

    Added fill property

--- projects/geomap/src/lib/basemap/basemap.component.ts
+++ projects/geomap/src/lib/basemap/basemap.component.ts
@@ -1,9 +1,10 @@
-import { ChangeDetectionStrategy, Component, Input, OnChanges } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { GeoProjection, geoPath } from 'd3-geo';
 import { FeatureCollection, Geometry } from 'geojson';
 
 export interface Properties {
   path?: string;
+  fill?: string;
 }
 
 @Component({
@@ -16,23 +17,31 @@ export class BasemapComponent implements OnChanges {
   @Input() basemap: FeatureCollection<Geometry, Properties>;
   @Input() projection: GeoProjection;
 
+  @Input() defaultFill: string;
+
   pathGenerator = geoPath();
   viewBox: string;
 
   constructor() { }
 
-  ngOnChanges(): void {
-    this.pathGenerator.projection(this.projection || null);
+  ngOnChanges(changes: SimpleChanges): void {
+    if ('defaultFill' in changes && !this.defaultFill) {
+      this.defaultFill = 'black';
+    }
 
-    if (this.basemap) {
-      const [[x0, y0], [x1, y1]] = this.pathGenerator.bounds(this.basemap);
-      const width = x1 - x0;
-      const height = y1 - y0;
-      this.viewBox = `${x0},${y0},${width},${height}`;
+    if ('basemap' in changes || 'projection' in changes) {
+      this.pathGenerator.projection(this.projection || null);
 
-      this.basemap.features.forEach(feature => {
-        feature.properties.path = this.pathGenerator(feature);
-      });
+      if (this.basemap) {
+        const [[x0, y0], [x1, y1]] = this.pathGenerator.bounds(this.basemap);
+        const width = x1 - x0;
+        const height = y1 - y0;
+        this.viewBox = `${x0},${y0},${width},${height}`;
+
+        this.basemap.features.forEach(feature => {
+          feature.properties.path = this.pathGenerator(feature);
+        });
+      }
     }
   }
 }

4ac80616c6f576f017e6b2f5721520da14f841f6
axdanbol <daniel_bolin@yahoo.com>
2018-11-05 15:35:06 -0500

    Improved basemap property updates

--- projects/geomap/src/lib/basemap/basemap.component.ts
+++ projects/geomap/src/lib/basemap/basemap.component.ts
@@ -1,22 +1,13 @@
 import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { GeoProjection, geoPath } from 'd3-geo';
 import { Feature, FeatureCollection, Geometry } from 'geojson';
-import { defaultTo } from 'lodash';
+import { defaultTo, forOwn, pick } from 'lodash';
 
 import { BoundField } from '@ngx-dino/core';
 
 export interface Properties {
   path?: string;
   fill?: string;
-}
-
-function createFieldApplier<T>(
-  field: BoundField<T>, defaultValue?: T
-): (feature: Feature) => T {
-  if (field) {
-    return feature => defaultTo(field.get(feature), defaultValue);
-  }
-  return () => defaultValue;
 }
 
 @Component({
@@ -32,37 +23,56 @@ export class BasemapComponent implements OnChanges {
   @Input() defaultFill: string;
   @Input() fillField: BoundField<string>;
 
-  pathGenerator = geoPath();
   viewBox: string;
+
+  private readonly pathGenerator = geoPath();
+  private readonly propertyOperators: { [prop: string]: (feature: Feature) => any } = {
+    path: (feature: Feature) => this.pathGenerator(feature),
+    fill: (feature: Feature) => {
+      const { defaultFill, fillField } = this;
+      return fillField ? defaultTo(fillField.get(feature), defaultFill) : defaultFill;
+    }
+  };
 
   constructor() { }
 
   ngOnChanges(changes: SimpleChanges): void {
+    // Set (reset) default value
     if ('defaultFill' in changes && !this.defaultFill) {
-      this.defaultFill = 'black'; // Set default value
+      this.defaultFill = 'black';
     }
 
-    if ('basemap' in changes || 'projection' in changes) {
-      this.pathGenerator.projection(this.projection || null);
+    // Properties that needs to be recomputed.
+    const ops: string[] = [];
 
-      if (this.basemap) {
-        // Calculate and set bounds
-        const [[x0, y0], [x1, y1]] = this.pathGenerator.bounds(this.basemap);
-        const width = x1 - x0;
-        const height = y1 - y0;
-        this.viewBox = `${x0},${y0},${width},${height}`;
+    // Order is important here!
+    // Projections must be applied before property computations.
+    if ('projection' in changes) {
+      this.pathGenerator.projection(this.projection || null); // Null -> identity projection
+      ops.push('path');
+    }
 
-        // Create operations
-        const fillOp = createFieldApplier(this.fillField, this.defaultFill);
+    if ('basemap' in changes && this.basemap) {
+      // Calculate and set bounds
+      const [[x0, y0], [x1, y1]] = this.pathGenerator.bounds(this.basemap);
+      const width = x1 - x0;
+      const height = y1 - y0;
+      this.viewBox = `${x0},${y0},${width},${height}`;
 
-        // Compute properties
-        this.basemap.features.forEach(feature => {
-          const { properties } = feature;
+      // Recompute all properties
+      ops.push(...Object.keys(this.propertyOperators));
+    }
 
-          properties.path = this.pathGenerator(feature);
-          properties.fill = fillOp(feature);
-        });
-      }
+    if ('fillField' in changes || 'defaultFill' in changes) {
+      ops.push('fill');
+    }
+
+    // Recompute properties
+    if (this.basemap) {
+      const selectedPropertyOperators = pick(this.propertyOperators, ops);
+      this.basemap.features.forEach(feature => forOwn(selectedPropertyOperators, (op, prop) => {
+        feature.properties[prop] = op(feature);
+      }));
     }
   }
 }
--- projects/geomap/src/lib/geomap.module.ts
+++ projects/geomap/src/lib/geomap.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { CoreModule } from '@ngx-dino/core';
+import { NetworkModule } from '@ngx-dino/network';
 import { GeomapComponent } from './geomap.component';
 import { BasemapComponent } from './basemap/basemap.component';
 import { GeomapV2Component } from './geomap-v2/geomap-v2.component';
 
 @NgModule({
-  imports: [CommonModule, CoreModule],
+  imports: [CommonModule, CoreModule, NetworkModule],
   exports: [GeomapComponent, GeomapV2Component],
   declarations: [GeomapComponent, BasemapComponent, GeomapV2Component]
 })

f5f135ab2ffe176ad22cb0f8c96afd8afee6a243
axdanbol <daniel_bolin@yahoo.com>
2018-11-21 12:01:47 -0500

    Fixed a geomap test

--- projects/geomap/src/lib/zoom-selector/zoom-selector.component.spec.ts
+++ projects/geomap/src/lib/zoom-selector/zoom-selector.component.spec.ts
@@ -18,8 +18,4 @@ describe('ZoomSelectorComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
 });

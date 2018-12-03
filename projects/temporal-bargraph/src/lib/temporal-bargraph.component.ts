cc8cc0f74e78b846999f9b472e5a2b50b46dcf94
axdanbol <daniel_bolin@yahoo.com>
2018-12-03 09:29:40 -0500

    Moved files around

--- projects/temporal-bargraph/src/lib/temporal-bargraph.component.ts
+++ projects/temporal-bargraph/src/lib/temporal-bargraph.component.ts
@@ -2,18 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'dino-temporal-bargraph',
-  template: `
-    <p>
-      temporal-bargraph works!
-    </p>
-  `,
-  styles: []
+  templateUrl: './temporal-bargraph.component.html',
+  styleUrls: ['./temporal-bargraph.component.css']
 })
 export class TemporalBargraphComponent implements OnInit {
-
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() { }
 }
--- projects/temporal-bargraph/src/public_api.ts
+++ projects/temporal-bargraph/src/public_api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of temporal-bargraph
  */
 
-export * from './lib/temporal-bargraph.service';
 export * from './lib/temporal-bargraph.component';
 export * from './lib/temporal-bargraph.module';

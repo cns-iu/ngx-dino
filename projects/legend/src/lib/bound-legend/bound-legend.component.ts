13e73a580ee5c84a6a4a5c3e4d3cae95fca3e669
Bruce Herr II <bherr@indiana.edu>
2019-01-04 17:10:31 -0500

    Updated bound legend to use SummaryDataService like all the other new components. Bumped to v0.11.0.

--- projects/legend/src/lib/bound-legend/bound-legend.component.ts
+++ projects/legend/src/lib/bound-legend/bound-legend.component.ts
@@ -1,98 +1,58 @@
-import {
-   Component,
-   OnInit,
-    Input,
-    SimpleChanges,
-    OnChanges } from '@angular/core';
-
-import { RawChangeSet, BoundField, Datum, idSymbol } from '@ngx-dino/core';
-
-import * as d3Array from 'd3-array';
-
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Observable } from 'rxjs';
-import { LegendDataService } from '../shared/legend-data.service';
+import { RawChangeSet, BoundField } from '@ngx-dino/core';
+import { SummaryDataService, SummaryStatistics, DataItem } from '../shared/summary-data.service';
+import { some, isNumber } from 'lodash';
 
 @Component({
   selector: 'dino-bound-legend',
   templateUrl: './bound-legend.component.html',
   styleUrls: ['./bound-legend.component.scss'],
-  providers: [LegendDataService]
+  providers: [SummaryDataService]
 })
 export class BoundLegendComponent implements OnInit, OnChanges {
   @Input() dataStream: Observable<RawChangeSet<any>>;
 
-  @Input() sizeField: BoundField<string | number>;
   @Input() idField: BoundField<number | string>;
-  @Input() categoryField: BoundField<string>;
+  @Input() valueField: BoundField<number>;
+  @Input() inputField: BoundField<number | string>;
+  @Input() labelField: BoundField<string>;
+  @Input() orderField: BoundField<number>;
 
-  @Input() boundLegendType: string;
+  @Input() boundLegendType: 'start' | 'end';
+  stats: SummaryStatistics;
 
-
-  label = '0';
-  changeLabels = false;
-  data = [];
-
-  constructor(private dataService: LegendDataService) { }
+  constructor(private summaryDataService: SummaryDataService) { }
 
   ngOnInit() {
-    this.dataService.nodes.subscribe((data) => {
-      this.data = this.data.filter((e: any) => !data.remove
-      .some((obj: Datum<any>) => obj[idSymbol] === e.id)).concat(data.insert.toArray() as any);
+    this.summaryDataService.observeStatistics().subscribe(this.setStatistics.bind(this));
+  }
 
-      data.update.forEach((el: any) => { // TODO typing for el
-        const index = this.data.findIndex((e) => e.id === el[idSymbol]);
-        if (index !== -1) {
-          this.data[index] = Object.assign(this.data[index] || {}, el );
-        }
-      });
-
-      data.replace.forEach((el: any) => { // TODO typing for el
-        const index = this.data.findIndex((e) => e.id === el[idSymbol]);
-        if (index !== -1) {
-          this.data[index] = el;
-        }
-      });
-      if (this.changeLabels && this.data.length > 0) {
-        this.calculateBoundLabels(this.data);
-      }
-    });
-   }
-
-  ngOnChanges(changes: SimpleChanges) {
-    if ('dataStream' in changes && this.dataStream) {
-      this.data = [];
-      this.updateStreamProcessor(false);
-    }
-    if ('sizeField' in changes) {
-        this.updateStreamProcessor(true, this.sizeField);
-        this.changeLabels = true;
+  private setStatistics(stats: SummaryStatistics) {
+    if (!stats || !stats.min) {
+      this.stats = null;
+    } else {
+      this.stats = stats;
     }
   }
 
-  updateStreamProcessor(update = true, changedField?: BoundField<number | string>) {
-    if (update && changedField) {
-      this.dataService.updateData(changedField);
-    }
-    if (!update) {
-      this.dataService.fetchData(
-        this.dataStream,
-        this.idField,
-        this.sizeField,
-        undefined,
-        this.categoryField ? this.categoryField : undefined,
-
-        // TODO
-        undefined,
-        undefined,
-        undefined,
-
-      );
+  ngOnChanges(changes: SimpleChanges): void {
+    if ('dataStream' in changes || 'idField' in changes) {
+      this.createProcessor();
+    } else if (some(changes, (_value, key) => /^\w+Field$/.test(key))) {
+      this.updateProcessor();
     }
   }
-
-  calculateBoundLabels(data: Datum[]) {
-    const label =  this.boundLegendType === 'start' ? Math.round(parseInt(d3Array.min(data, (d: any) => d.size), 10)) :
-     Math.round(parseInt(d3Array.max(data, (d: any) => d.size), 10));
-    this.label = (!isNaN(label)) ? label.toString() : '';
+  private createProcessor() {
+    if (!this.dataStream || !this.idField) {
+      this.summaryDataService.clearProcessor();
+    } else {
+      this.summaryDataService.createProcessor(this.dataStream, this.idField,
+        this.valueField, this.inputField, this.labelField, this.orderField);
+    }
+  }
+  private updateProcessor() {
+    this.summaryDataService.updateProcessor(
+      this.valueField, this.inputField, this.labelField, this.orderField);
   }
 }

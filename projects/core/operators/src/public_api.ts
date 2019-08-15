3814584ca531d62351c8dd5bde2673032a0a38c7
axdanbol <daniel_bolin@yahoo.com>
2019-08-15 09:20:09 -0400

    Added combine operator

--- projects/core/operators/src/public_api.ts
+++ projects/core/operators/src/public_api.ts
@@ -2,7 +2,8 @@
  * Public API Surface of operators
  */
 
-export { access } from './lib/access';
+export { access, access as pick } from './lib/access';
+export { combine } from './lib/combine';
 export { constant } from './lib/constant';
 export { map } from './lib/map';
 export { pipe } from './lib/pipe';
